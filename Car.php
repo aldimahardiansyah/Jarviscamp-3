337441746ea6a268be773798f6eecf8a2c992e3f
aldimahardiansyah <aldimdiansyah@gmail.com>
2022-10-08 08:43:03 +0700

    pertemuan 4 jarvsicamp batch 4

---
+++ Car.php
@@ -0,0 +1,22 @@
+<?php
+class Car
+{
+    // Constructor
+    public function __construct()
+    {
+        echo "Ini konstruktor\n";
+    }
+
+    // Atribut
+    public $color = "Red";
+    public $max_speed = 300;
+
+    // Method
+    public function jalan()
+    {
+        echo "Mobil berjalan...\n";
+    }
+}
+
+$avanza = new Car;
+$avanza->jalan();
