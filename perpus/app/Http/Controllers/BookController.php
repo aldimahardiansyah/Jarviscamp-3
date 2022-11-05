f5b2a707cb19414f16c4b1b60a21fe64416fea64
aldimdiansyah <aldimdiansyah@gmail.com>
2022-11-05 08:08:13 +0700

    add update resource

--- perpus/app/Http/Controllers/BookController.php
+++ perpus/app/Http/Controllers/BookController.php
@@ -63,4 +63,41 @@ class BookController extends Controller
             'data' => $book
         ], 200);
     }
+
+    function update($id, Request $request)
+    {
+        /**
+         * Alur update resource:
+         * 1. menangkap id & data request body
+         * 2. mendapatkan data book berdasarkan id
+         * 3. deklarasikan data yang akan di update
+         * 4. kirim status 200 jika berhasil diupdate
+         */
+
+        $book = Book::find($id);
+
+        // jika id tidak ditemukan
+        if (!$book) {
+            return response()->json([
+                'message' => 'Resource not found',
+                'status' => 404,
+            ], 404);
+        }
+
+        $updated = $book->update([
+            'title' => $request->title ?? $book->title,
+            'author' => $request->author ?? $book->author,
+            'publisher' => $request->publisher ?? $book->publisher,
+            'rating' => $request->rating ?? $book->rating,
+            'published_date' => $request->published_date ?? $book->published_date
+        ]);
+
+        if ($updated) {
+            return response()->json([
+                'message' => 'Data updated successfully',
+                'data' => $updated,
+                'status' => 200
+            ], 200);
+        }
+    }
 }
